99f77b25a38bf850f484e41d74794973bca07090
sedric <sedric@student.21-school.ru>
2021-04-28 00:24:19 +0300

    Phonebook

--- Module_00/ex01/megaphone.cpp
+++ Module_00/ex01/megaphone.cpp
@@ -1,18 +1,59 @@
 #include <iostream>
+#include <string>
 
-int main(int argv, char **argc)
-{    
-    int i;
-    int j;
-    
-    if (argv == 1)
+class PhoneBook
+{
+    public:
+        PhoneBook();
+        void add();
+    private:
+        std::string str[2][11];
+};
+
+PhoneBook::PhoneBook()
+{
+    this->str[0][0] = "First name";
+    this->str[0][1] = "Last name";
+    this->str[0][2] = "Nickname";
+    this->str[0][3] = "Login";
+    this->str[0][4] = "Postal address";
+    this->str[0][5] = "Email address";
+    this->str[0][6] = "Phone number";
+    this->str[0][7] = "Birthday date";
+    this->str[0][8] = "Favorite meal";
+    this->str[0][9] = "Underwear color";
+    this->str[0][10] = "Darkest secret";
+}
+
+void PhoneBook::add()
+{
+    for (int i = 0; i < 10; i++)
     {
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
-        return(0);
+        std::cout << "Enter the" << this->str[0][i];
+        std::cout << "\n";
+        std::cin >> this->str[1][i];
+        std::cout << this->str[1][i];
+        std::cout << "\n";
+    }    
+}
+
+int main()
+{    
+    int i = 0;
+    std::string input;
+
+    PhoneBook MyPhoneBook[8];
+    while (1)
+    {
+        std::cin >> input;
+        if (input == "EXIT")
+            exit(0);
+        else if (input == "ADD")
+        {
+            MyPhoneBook[i].add();
+            i++;
+        }
     }
-    for (i = 1; argc[i] != NULL; i++)
-        for (j = 0; argc[i][j] != 0; j++)
-            std::cout << (char)toupper(argc[i][j]);
-    std::cout << "\n";
+//    MyPhoneBook.print();
     return(0);
 }
